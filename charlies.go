b9e56e5b230a20b59df3854fd4857e32f73cd0da
Gavin Bong <gb.javouhey@gmail.com>
2015-01-25 15:28:08 -0800

    blob 1

--- charlies.go
+++ charlies.go
@@ -2,15 +2,23 @@
 // Use of this source code is governed by the Hugsware
 // license that can be found in the LICENSE file.
 
-// Package charlieswithcrayons provides access to random.org's JSON RPC api
+// Package charlieswithcrayons provides access
+// to random.org's JSON RPC api.
 package charlieswithcrayons
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
+    "time"
+    "reflect"
+    "bytes"
 )
 
 const Version = "1.0.0"
 const StdUrl = "https://api.random.org/json-rpc/1/invoke"
+const methodBlob = "generateBlobs"
+const methodBlobSigned = "generateSignedBlobs"
 
 type Arguments struct {
 	ApiKey string
@@ -29,9 +37,51 @@ func New(args Arguments) *RandomOrgClient {
 	return client
 }
 
-func (c *RandomOrgClient) GetRandomBits(bitSize int, count int) error {
+type BlobParams struct {
+	ApiKey string `json:"apiKey"`
+	N      int    `json:"n"`
+	Size   int    `json:"size"`
+}
+
+type BlobArguments struct {
+	JsonRpc    string `json:"jsonrpc"`
+	Method     string `json:"method"`
+	BlobParams `json:"params"`
+	Id         int `json:"id"`
+}
+
+func (c *RandomOrgClient) newBlobArguments(method string, bitSize int, txId int) *BlobArguments {
+	retval := new(BlobArguments)
+	retval.JsonRpc = "2.0"
+	retval.Method = method
+	retval.Id = txId
+	retval.BlobParams.ApiKey = c.apiKey
+	retval.BlobParams.N = 1
+	retval.BlobParams.Size = bitSize
+	return retval
+}
+
+// You must have a valid Api key to invoke this method.
+// Argument `txId` can be used to differentiate calls.
+func (c *RandomOrgClient) GetRandomBits(bitSize int, count int, txId int) error {
 	log.Println("invoke generateBlobs")
-	return nil
+	blobArguments := c.newBlobArguments(methodBlob, bitSize, txId)
+	client := http.Client{
+		Timeout: time.Duration(2 * time.Second),
+	}
+
+	postBody, err := json.Marshal(blobArguments)
+    if err != nil {
+		panic(err)
+	}
+    log.Println(reflect.TypeOf(postBody))
+
+	req, err := http.NewRequest("POST", c.url, bytes.NewBuffer(postBody))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+    log.Println(resp)
+	return err
 }
 
 func (c *RandomOrgClient) GetSignedRandom(bitSize int, count int) error {
