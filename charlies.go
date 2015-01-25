0b1056e06df208fd12c3cd62ce9d6981a16cdd4c
Gavin Bong <gb.javouhey@gmail.com>
2015-01-24 21:24:13 -0800

    added a license file

--- charlies.go
+++ charlies.go
@@ -1,4 +1,17 @@
+// Copyright 2015 Gavin Bong. All rights reserved.
+// Use of this source code is governed by the Hugsware 
+// license that can be found in the LICENSE file.
+
 // Package charlieswithcrayons provides access to random.org's JSON RPC api
 package charlieswithcrayons
 
 const Hello = "Hello"
+
+type RandomOrgClient struct {
+  apiKey string
+}
+
+func New() *RandomOrgClient {
+  client := new(RandomOrgClient)
+  return client
+}
