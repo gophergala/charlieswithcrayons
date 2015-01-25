c2501d00b144a32337fb8ea8870042bb87a1e23f
Gavin Bong <gb.javouhey@gmail.com>
2015-01-25 10:42:08 -0800

    baby step

--- charlies.go
+++ charlies.go
@@ -1,17 +1,26 @@
 // Copyright 2015 Gavin Bong. All rights reserved.
-// Use of this source code is governed by the Hugsware 
+// Use of this source code is governed by the Hugsware
 // license that can be found in the LICENSE file.
 
 // Package charlieswithcrayons provides access to random.org's JSON RPC api
 package charlieswithcrayons
 
-const Hello = "Hello"
+const Version = "1.0.0"
+const StdUrl = "https://api.random.org/json-rpc/1/invoke"
 
-type RandomOrgClient struct {
-  apiKey string
+type Arguments struct {
+	ApiKey string
+	Url    string
 }
 
-func New() *RandomOrgClient {
-  client := new(RandomOrgClient)
-  return client
+type RandomOrgClient struct {
+	apiKey string
+	url    string
+}
+
+func New(args Arguments) *RandomOrgClient {
+	client := new(RandomOrgClient)
+	client.apiKey = args.ApiKey
+	client.url = args.Url
+	return client
 }
